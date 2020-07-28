5a0769a4fddca9424e55f96ab240c5ee4a669008
Thomas Ménès <thomas.menes@hotmail.com>
2020-07-29 01:13:45 +0200

    pep8

--- utils/character.py
+++ utils/character.py
@@ -10,6 +10,7 @@ We update MacGyver's x and y if the provisionary position is a passage, the star
 If the new block contains an item, we update MacGyver's "items_found" list
 If all items are found, we update MacGyver's "items_found" list to the syringe
 """
+
 # Third party imports
 import pygame
 
